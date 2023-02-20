421c5f9f70238f20ec004101868f12082308a1a0
cjones1047 <cjmj1047@gmail.com>
2023-02-20 07:05:05 -0600

    Movie table schema created inside movie_collection.db

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movie_collection.db"
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Movie %r>' % self.title
 
 
 @app.route("/")
