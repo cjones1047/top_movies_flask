007a9ffa88b6274f56c0fe943dc8d13814bad3e2
cjones1047 <cjmj1047@gmail.com>
2023-02-20 07:52:54 -0600

    RateMovieForm created from FlaskForm; RateMovieForm rendered when edit_movie route hit; edit_movie route is passed a certain movie as a parameter; after RateMovieForm user submission and validation, entry in database with id correlating to movie_id is updated and user is redirected to home route

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, FloatField
+from wtforms.validators import DataRequired, NumberRange
 import requests
 
 app = Flask(__name__)
@@ -27,11 +27,31 @@ class Movie(db.Model):
         return '<Movie %r>' % self.title
 
 
+class RateMovieForm(FlaskForm):
+    rating = FloatField('Your Rating (Out of 10)', validators=[NumberRange(min=0, max=10), DataRequired()],
+                        render_kw={"placeholder": "e.g. 7.5"})
+    review = StringField('Your Review', validators=[DataRequired()], render_kw={"placeholder": '"I think it was..."'})
+    submit = SubmitField('Change Rating')
+
+
 @app.route("/")
 def home():
     movies = db.session.query(Movie).all()
 
     return render_template("index.html", movies=movies)
+
+
+@app.route("/edit_movie/<movie_id>", methods=["GET", "POST"])
+def edit_movie(movie_id):
+    form = RateMovieForm()
+    movie_to_update = db.session.get(Movie, movie_id)
+    if form.validate_on_submit():
+        movie_to_update.rating = form.rating.data
+        movie_to_update.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('edit.html', form=form)
 
 
 if __name__ == '__main__':
